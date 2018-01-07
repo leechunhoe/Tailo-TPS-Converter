5c89daaddd6eb7a6da7ccb127b369620c8c48cb6
Lee Chun Hoe <leechunhoe0625@gmail.com>
2018-01-07 21:36:54 +0800

    Take file name argument for run.py program

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
+import sys
 from stailo_hongim_converter import StailoHongimConverter
-StailoHongimConverter().convert_file_full("input.txt")
+
+if len(sys.argv) == 2:
+	filename = sys.argv[-1]
+	StailoHongimConverter().convert_file_full(filename)
+else:
+	print "Please provide one input file name!"
